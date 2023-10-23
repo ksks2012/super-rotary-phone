94fc510de5c7f28944e9e2bb63d3b0cee2546745
ksks2012 <st890751999@gmail.com>
2023-10-23 23:00:54 +0800

    test(test_test): Add stage in TestFailAgree2B
    - Connect
    - Disconnect

--- test_test.go
+++ test_test.go
@@ -195,6 +195,7 @@ func TestFailAgree2B(t *testing.T) {
 	// disconnect one follower from the network.
 	leader := cfg.checkOneLeader()
 	cfg.disconnect((leader + 1) % servers)
+	fmt.Printf("============= disconnect: %v =============\n", (leader+1)%servers)
 
 	// the leader and remaining follower should be
 	// able to agree despite the disconnected follower.
@@ -206,6 +207,7 @@ func TestFailAgree2B(t *testing.T) {
 
 	// re-connect
 	cfg.connect((leader + 1) % servers)
+	fmt.Printf("============= connect: %v =============\n", (leader+1)%servers)
 
 	// the full set of servers should preserve
 	// previous agreements, and be able to agree
